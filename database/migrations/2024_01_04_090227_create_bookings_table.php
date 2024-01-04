2a1a393237bfb6978bf1257ef4a0dcf47b397e19
Jan Patrick Maureal <janpatrickmaureal@gmail.com>
2024-01-04 17:17:20 +0800

    update booking migration

--- database/migrations/2024_01_04_090227_create_bookings_table.php
+++ database/migrations/2024_01_04_090227_create_bookings_table.php
@@ -6,22 +6,29 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('bookings', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void
+	{
+		Schema::create('bookings', function (Blueprint $table) {
+			$table->uuid('id')->primary();
+			$table->foreignUuid('customer_id')->references('id')->on('customers')->onDelete('cascade');
+			$table->foreignUuid('activity_id')->references('id')->on('activities')->onDelete('cascade');
+			$table->date('booking_date');
+			$table->date('checkin_date');
+			$table->date('checkout_date');
+			$table->string('booking_status')->default('pending');
+			$table->string('payment_status')->default('pending');
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('bookings');
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void
+	{
+		Schema::dropIfExists('bookings');
+	}
 };
