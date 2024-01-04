bf0c4bee0ed0c0e57409aef21663b6355557042b
Jan Patrick Maureal <janpatrickmaureal@gmail.com>
2024-01-05 00:35:00 +0800

    Update Favicon and homepage

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>HeroBiz Bootstrap Template - Home 3</title>
+  <title>Lakat Balas Beach Adventure Camp</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
