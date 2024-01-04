ca613662f9ee398457aa0e4a171c40919c9a0365
Jan Patrick Maureal <janpatrickmaureal@gmail.com>
2024-01-04 22:46:10 +0800

    Add Herobiz Theme for Homepage

---
+++ resources/views/homepage/contact.blade.php
@@ -0,0 +1,81 @@
+<!-- ======= Contact Section ======= -->
+	<section id="contact" class="contact">
+		<div class="container">
+
+			<div class="section-header">
+				<h2>Contact Us</h2>
+			</div>
+
+		</div>
+
+		<div class="map">
+			<iframe src="https://www.google.com/maps/embed/v1/place?q=Lakat+Beach&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8" frameborder="0" allowfullscreen></iframe>
+		</div><!-- End Google Maps -->
+
+		<div class="container">
+
+			<div class="row gy-5 gx-lg-5">
+
+				<div class="col-lg-4">
+
+					<div class="info">
+						<h3>Get in touch</h3>
+						<p>You may contact us.</p>
+						<div class="info-item d-flex">
+							<i class="bi bi-geo-alt flex-shrink-0"></i>
+							<div>
+								<h4>Location:</h4>
+								<p>Lakat Balas,Barangay Owak,Asturias, Cebu City, Philippines</p>
+							</div>
+						</div><!-- End Info Item -->
+
+						<div class="info-item d-flex">
+							<i class="bi bi-envelope flex-shrink-0"></i>
+							<div>
+								<h4>Email:</h4>
+								<p>info@example.com</p>
+							</div>
+						</div><!-- End Info Item -->
+
+						<div class="info-item d-flex">
+							<i class="bi bi-phone flex-shrink-0"></i>
+							<div>
+								<h4>Call:</h4>
+								<p>+1 5589 55488 55</p>
+							</div>
+						</div><!-- End Info Item -->
+
+					</div>
+
+				</div>
+
+				<div class="col-lg-8">
+					<form action="forms/contact.php" method="post" role="form" class="php-email-form">
+						<div class="row">
+							<div class="col-md-6 form-group">
+								<input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+							</div>
+							<div class="col-md-6 form-group mt-3 mt-md-0">
+								<input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+							</div>
+						</div>
+						<div class="form-group mt-3">
+							<input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+						</div>
+						<div class="form-group mt-3">
+							<textarea class="form-control" name="message" placeholder="Message" required></textarea>
+						</div>
+						<div class="my-3">
+							<div class="loading">Loading</div>
+							<div class="error-message"></div>
+							<div class="sent-message">Your message has been sent. Thank you!</div>
+						</div>
+						<div class="text-center"><button type="submit">Send Message</button></div>
+					</form>
+				</div><!-- End Contact Form -->
+
+			</div>
+
+		</div>
+	</section>
+<!-- End Contact Section -->
---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>HeroBiz Bootstrap Template - Home 3</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="{{ asset('img/herobiz/favicon.png') }}" rel="icon">
+  <link href="{{ asset('img/herobiz/apple-touch-icon.png') }}" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="{{ asset('vendor/bootstrap/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+  <link href="{{ asset('vendor/aos/aos.css') }}" rel="stylesheet">
+  <link href="{{ asset('vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+
+  <!-- Variables CSS Files. Uncomment your preferred color scheme -->
+  <link href="{{ asset('theme/herobiz/css/variables.css') }}" rel="stylesheet">
+  <!-- <link href="assets/css/variables-blue.css" rel="stylesheet"> -->
+  <!-- <link href="assets/css/variables-green.css" rel="stylesheet"> -->
+  <!-- <link href="assets/css/variables-orange.css" rel="stylesheet"> -->
+  <!-- <link href="assets/css/variables-purple.css" rel="stylesheet"> -->
+  <!-- <link href="assets/css/variables-red.css" rel="stylesheet"> -->
+  <!-- <link href="assets/css/variables-pink.css" rel="stylesheet"> -->
+
+  <!-- Template Main CSS File -->
+  <link href="{{ asset('theme/herobiz/css/main.css') }}" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: HeroBiz
+  * Updated: Sep 18 2023 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/herobiz-bootstrap-business-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+	@include('layouts.partials.home.header')
+
+	@include('layouts.partials.home.hero')
+
+
+
+  <main id="main">
+		{{ $slot }}
+  </main>
+
+	@include('layouts.partials.home.footer')
+
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('vendor/aos/aos.js') }}"></script>
+  <script src="{{ asset('vendor/glightbox/js/glightbox.min.js') }}"></script>
+  <script src="{{ asset('vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+  <script src="{{ asset('vendor/swiper/swiper-bundle.min.js') }}"></script>
+  <script src="{{ asset('vendor/php-email-form/validate.js') }}"></script>
+
+  <!-- Template Main JS File -->
+  <script src="{{ asset('theme/herobiz/js/main.js') }}"></script>
+
+</body>
+
+</html>
