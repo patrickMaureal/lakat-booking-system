97c6088729962c33bec123ca33f62c4179d5ad68
Jan Patrick Maureal <janpatrickmaureal@gmail.com>
2024-01-04 20:45:18 +0800

    Update Booking

--- resources/views/booking/index.blade.php
+++ resources/views/booking/index.blade.php
@@ -11,12 +11,12 @@
 
 	<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
 		<div class="d-block mb-md-0">
-			<h2 class="h4">Customers</h2>
+			<h2 class="h4">List of Booking</h2>
 		</div>
 		<div class="btn-toolbar mb-2 mb-md-0">
-			<a href="{{ route('customers.create') }}" class="btn btn-sm btn-gray-800 d-inline-flex align-items-center">
+			<a href="{{ route('bookings.create') }}" class="btn btn-sm btn-gray-800 d-inline-flex align-items-center">
 				<i class="icon icon-xs me-2 bi bi-plus-lg"></i>
-				Add Customer
+				Add Booking
 			</a>
 		</div>
 	</div>
