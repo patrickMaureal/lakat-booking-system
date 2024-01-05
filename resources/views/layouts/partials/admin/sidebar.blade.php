15a2c633557d3195caaf302f7d842d99d06103c7
Jan Patrick Maureal <janpatrickmaureal@gmail.com>
2024-01-06 01:07:21 +0800

    Update Favicon and logo

--- resources/views/layouts/partials/admin/sidebar.blade.php
+++ resources/views/layouts/partials/admin/sidebar.blade.php
@@ -1,8 +1,8 @@
 {{-- sidebar toggle --}}
 <nav class="navbar navbar-dark navbar-theme-primary px-4 col-12 d-lg-none">
 	<a class="navbar-brand me-lg-5" href="{{ route('dashboard') }}">
-		<img class="navbar-brand-dark" src="{{ asset('img/volt/brand/light.svg') }}" alt="Volt logo" />
-		<img class="navbar-brand-light" src="{{ asset('img/volt/brand/dark.svg') }}" alt="Volt logo" />
+		<img class="navbar-brand-dark" src="{{ asset('img/lakat-balas/lakat-logo.png') }}" alt="Volt logo" />
+		<img class="navbar-brand-light" src="{{ asset('img/lakat-balas/lakat-logo.png') }}" alt="Volt logo" />
 	</a>
 	<div class="d-flex align-items-center">
 		<button class="navbar-toggler d-lg-none collapsed" type="button" data-bs-toggle="collapse"
@@ -45,7 +45,7 @@
 			<li class="nav-item">
 				<a href="{{ route('dashboard') }}" class="nav-link d-flex align-items-center">
 					<span class="sidebar-icon">
-						<img src="{{ asset('img/volt/brand/light.svg') }}" height="20" width="20" alt="Volt Logo">
+						<img src="{{ asset('img/lakat-balas/lakat-logo.png') }}" height="20" width="20" alt="Volt Logo">
 					</span>
 					<span class="mt-1 ms-1 sidebar-text">Lakat Beach</span>
 				</a>
