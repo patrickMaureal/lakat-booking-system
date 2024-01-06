0c12e08052c74f5c72f808f4fd62c1349ff49e2c
Jan Patrick Maureal <janpatrickmaureal@gmail.com>
2024-01-06 20:54:24 +0800

    Add Datatable Payments

---
+++ public/js/admin/page/payment/index.js
@@ -0,0 +1,84 @@
+$(function () {
+  // holds the id when button delete click
+  let this_id;
+  // modal
+  let modal = $('#payment-modal');
+	// start => datatable
+	let table = $("#payment-table").DataTable({
+		autoWidth: false,
+		responsive: true,
+		processing: true,
+		serverSide: true,
+		searching: true,
+		paging: true,
+		ajax: {
+			method: "GET",
+			url: "/payments/table",
+			dataType: "json",
+			error: function (errors) {
+				toast.fire({
+					icon: 'error',
+					title: 'Invalid Data Token.',
+				})
+			},
+		},
+		language: {
+			searchPlaceholder: "Search Records..",
+		},
+		columns: [
+			{ data: "booking_id", name: "booking_id" },
+			{ data: "amount", name: "amount" },
+			{ data: "created_at", name: "created_at" },
+			{
+				data: "action",
+				name: "action",
+				orderable: false,
+				searchable: false,
+			},
+		],
+	});
+	// end
+	// custom search
+	$("#custom-search-field").keyup(function () {
+		table.search($(this).val()).draw();
+	});
+	// custom page length
+	$("#custom-page-length").change(function () {
+		table.page.len($(this).val()).draw();
+	}).trigger('change');
+  // start => button delete
+	$('body').on('click', '.delete-payment', function () {
+		this_id = $(this).attr('data-id');
+		modal.modal('show');
+	});
+	// end
+	//start => modal button delete
+	$('body').on('click', '#destroy-payment', function () {
+		$.ajax({
+			type: 'DELETE',
+			url: '/payments/' + this_id,
+			dataType: 'json',
+			beforeSend : function() {
+				buttons('destroy-payment', 'start');
+			}
+		})
+		.done(function (response) {
+			table.ajax.reload();
+			toast.fire({
+				icon: 'success',
+				title: response.message,
+			});
+		})
+		.fail(function (jqXHR, textStatus, errorThrown) {
+			toast.fire({
+				icon: 'error',
+				title: jqXHR.responseJSON.message,
+			});
+		})
+		.always(function (jqXHROrData, textStatus, jqXHROrErrorThrown){
+			buttons('destroy-payment', 'finish');
+			modal.modal('hide');
+		});
+	});
+	// end
+});
--- resources/views/payment/index.blade.php
+++ resources/views/payment/index.blade.php
@@ -1,110 +1,61 @@
 <x-app-layout>
 
-	{{-- per page status --}}
-	@if ( session('status') )
-		<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
-			<div class="alert alert-success" role="alert">
-				{{ session('status') }}
-			</div>
-		</div>
-	@endif
-
 	<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
 		<div class="d-block mb-md-0">
 			<h2 class="h4">List of Payments</h2>
 		</div>
 		<div class="btn-toolbar mb-2 mb-md-0">
-			{{-- <a href="{{ route('customers.create') }}" class="btn btn-sm btn-gray-800 d-inline-flex align-items-center">
+			<a href="{{ route('payments.create') }}" class="btn btn-sm btn-gray-800 d-inline-flex align-items-center">
 				<i class="icon icon-xs me-2 bi bi-plus-lg"></i>
-				Add Customer
-			</a> --}}
+				Add Payment
+			</a>
 		</div>
 	</div>
 	<div class="table-settings mb-4">
 		<div class="row align-items-center justify-content-between">
-			{{-- <div class="col col-md-6 col-lg-3 col-xl-4">
-				<form action="{{ route('customers.index') }}" method="GET">
-					<div class="input-group me-2 me-lg-3 fmxw-400">
-						<input type="text" name="search" value="{{ $searchVal }}" class="form-control" placeholder="Search name">
-						<span class="input-group-text">
-							<button type="submit" class="btn btn-xs">
-								<i class="icon fs-6 bi bi-search"></i>
-							</button>
-						</span>
-					</div>
-				</form>
-			</div> --}}
+			<div class="col col-md-6 col-lg-3 col-xl-4">
+				<div class="input-group me-2 me-lg-3 fmxw-400">
+					<span class="input-group-text">
+						<svg class="icon icon-xs" x-description="Heroicon name: solid/search" xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+							<path fill-rule="evenodd"
+								d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+								clip-rule="evenodd"></path>
+						</svg>
+					</span>
+					<input id="custom-search-field" type="text" class="form-control" placeholder="Search user..">
+				</div>
+			</div>
+			<div class="col-4 col-md-2 col-xl-1 ps-md-0 text-end">
+				<select id="custom-page-length" class="form-select fmxw-200 d-md-inline" aria-label="Message select example 2">
+					<option value="5" selected>5</option>
+					<option value="10">10</option>
+					<option value="50">50</option>
+					<option value="100">100</option>
+				</select>
+			</div>
 		</div>
 	</div>
 	<div class="card card-body border-0 shadow table-wrapper table-responsive mb-5">
 		<table class="table table-hover">
 			<thead>
 				<tr>
-					<th class="border-gray-200">No.</th>
-					<th class="border-gray-200">Guest</th>
-					<th class="border-gray-200">Mode of Payment</th>
-					<th class="border-gray-200">Reference No.</th>
-					<th class="border-gray-200">Status</th>
-					<th class="border-gray-200">Action</th>
+					<th>Booking ID</th>
+					<th>Amount</th>
+					<th>Transaction Date</th>
+					<th>Action</th>
 				</tr>
 			</thead>
 			<tbody>
-				{{-- @forelse ($customers as $customer)
-					<tr>
-						<td valign="middle"><span class="fw-normal">{{ $loop->iteration }}</span></td>
-						<td valign="middle">
-							<span class="fw-normal">{{ $customer->first_name . ' ' . $customer->last_name }}</span>
-						</td>
-						<td valign="middle"><span class="fw-normal">{{ $customer->email }}</span></td>
-						<td valign="middle"><span class="fw-normal">{{ $customer->phone_number }}</span></td>
-						<td valign="middle">
-							<a href="{{ route('customers.edit', $customer->id) }}" class="btn btn-sm btn-pill btn-outline-tertiary">Edit</a>
-
-							<button class="btn btn-sm btn-pill btn-outline-danger" type="button" data-bs-toggle="modal" data-bs-target="#modal-delete-{{ $customer->id }}">Delete</button>
-
-
-							<div class="modal fade" id="modal-delete-{{ $customer->id }}" tabindex="-1" role="dialog" aria-labelledby="modal-default" aria-hidden="true">
-								<div class="modal-dialog modal-dialog-centered" role="document">
-									<div class="modal-content">
-										<form action="{{ route('customers.destroy', $customer->id) }}" method="POST">
-
-											@csrf
-											@method('DELETE')
-
-											<div class="modal-header">
-												<h2 class="h6 modal-title">Confirmation</h2>
-												<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-											</div>
-											<div class="modal-body">
-												<p>Are you sure you want to delete this customer?</p>
-											</div>
-											<div class="modal-footer">
-												<button type="submit" class="btn btn-danger">Yes</button>
-												<button type="button" class="btn btn-link text-gray-600 ms-auto" data-bs-dismiss="modal">Close</button>
-											</div>
-										</form>
-									</div>
-								</div>
-							</div>
-
-						</td>
-					</tr>
-				@empty
-					<tr class="text-center">
-						<td colspan="5">No data.</td>
-					</tr>
-				@endforelse --}}
-
 			</tbody>
 		</table>
-
-		<div class="card-footer px-3 border-0 d-flex flex-column flex-lg-row align-items-center justify-content-between">
-
-			{{-- pagination --}}
-			{{-- {{ $customers->links('vendor.pagination.bootstrap-5') }} --}}
-
-		</div>
-
 	</div>
+
+	{{-- modal --}}
+	<x-modal modal-id="payment-modal" button-id="destroy-payment" type="delete" label="Payment" />
+
+	@push('scripts')
+		<script type="text/javascript" src="{{ asset('js/admin/page/payment/index.js') }}"></script>
+	@endpush
 
 </x-app-layout>
